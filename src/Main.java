56c47fee84f70afedb1ea9b5c5702a1fbe2bdcf1
Grampaoli <grampaolileandro@gmail.com>
2025-05-30 14:07:06 -0300

    Hola mundo primer commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pedaso de pedasin");
-        System.out.println("p de penelope");
-        
+        System.out.println("Hola mundo commit");
+
     }
 }
